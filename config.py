e75d851806d54f4b1832ae3cda50eb2a05d01e01
Annihilater <yanmiexingkong@gmail.com>
2019-11-05 09:10:16 +0800

    add config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date  : 2019/10/28 17:44
+# @Author: yanmiexingkong
+# @email : yanmiexingkong@gmail.com
+# @File  : config.py
+
+# excel 表格的标题
+EXCEL_TITLE = ['国家', '省', '市', '区', '部门', '网址', '搜索网址', '关键字1', '搜索结果1', '关键字2', '搜索结果2', '关键字3', '搜索结果3']
+NEW_COL = ['部门', '网址', '搜索网址', '关键字', '搜索结果']
