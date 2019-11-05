94f188654e153536750b844d64978d79f26bf7b9
Annihilater <yanmiexingkong@gmail.com>
2019-11-05 09:18:25 +0800

    test os.path.exists()

---
+++ test/12.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date  : 2019/10/29 16:54
+# @Author: PythonVampire
+# @email : vampire@ivamp.cn
+# @File  : 12.py
+import os
+
+print(os.path.exists('../data/pdf/110.pdf'))
