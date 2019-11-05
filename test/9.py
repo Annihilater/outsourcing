0b30875e53429f7aac9a9a816b8f1fffd87d5102
Annihilater <yanmiexingkong@gmail.com>
2019-11-05 09:17:43 +0800

    test logging

---
+++ test/9.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date  : 2019/10/28 15:49
+# @Author: yanmiexingkong
+# @email : yanmiexingkong@gmail.com
+# @File  : 9.py
+
+
+import logging
+from logging import handlers
+
+
+class Logger(object):
+    # 日志级别关系映射
+    level_relations = {
+        'debug': logging.DEBUG,
+        'info': logging.INFO,
+        'warning': logging.WARNING,
+        'error': logging.ERROR,
+        'crit': logging.CRITICAL
+    }
+
+    def __init__(self, filename, level='info', when='D', back_count=3,
+                 fmt='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s', ):
+        self.logger = logging.getLogger(filename)
+        self.logger.setLevel(self.level_relations.get(level))  # 设置日志级别
+
+        sh = logging.StreamHandler()  # 往屏幕上输出
+        format_str = logging.Formatter(fmt)  # 设置日志格式
+        sh.setFormatter(format_str)  # 设置屏幕上显示的格式
+        self.logger.addHandler(sh)  # 把对象加到logger里
+
+        # 往文件里写入指定时间间隔的自动生成文件处理器，实例化TimedRotatingFileHandler
+        # interval是时间间隔，backupCount是备份文件的个数，如果超过这个个数，就会自动删除，when是间隔的时间单位
+        # 单位有以下几种：S 秒、M 分、H 小时、D 天、W 每星期（interval==0时代表星期一）、midnight 每天凌晨
+        th = handlers.TimedRotatingFileHandler(filename=filename, when=when, backupCount=back_count, encoding='utf-8')
+        th.setFormatter(format_str)  # 设置文件里写入的格式
+        self.logger.addHandler(th)  # 把对象加到 logger 里
+
+
+if __name__ == '__main__':
+    log = Logger('all.log', level='debug')
+    log.logger.debug('debug')
+    log.logger.info('info')
+    log.logger.warning('警告')
+    log.logger.error('报错')
+    log.logger.critical('严重')
+    Logger('error.log', level='error').logger.error('error')
